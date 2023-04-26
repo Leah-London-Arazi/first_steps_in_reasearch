29d562b4356f67b15d32db942876146ccb1380ce
Leah-London-Arazi <leahl@mail.tau.ac.il>
2023-04-26 18:41:06 +0300

    now works but have a bug, about to do some major change

--- expirements/print_riddle_matrix.py
+++ expirements/print_riddle_matrix.py
@@ -1,25 +1,57 @@
 import numpy as np
 import pandas as pd
 
-class ProductTerm(object):
-    def __init__(self):
-        pass
-    def diff_digits(self):
-        pass
-    def is_complement(self, other):
-        '''
-        if complement will return the different digit, else 1
-        '''
-        pass
-    def is_contained(self, other):
-        pass
-    def remove_literal(self, digit):
-        pass
-    def merge(self, other):
-        '''
-        new_input, drop_input, diff_digit
-        '''
-        pass
+# bits methods
+def compare_bits(bit, other_bit):
+    '''
+    returns 0 if equal.
+    returns 1 if bit > other_bit.
+    returns -1 if bit < other_bit.
+    returns None if the bits are stable and different.
+    '''
+    if bit == other_bit:
+        return 0
+    if bit == 'u':
+        return -1
+    if other_bit == 'u':
+        return 1
+    return None
+def diff_digits(term1, term2):
+    return DiffDigits([i for i in range(len(term1)) if compare_bits(term1[i], term2[i]) == None])
+def is_contained(term1, term2):
+    '''
+    returns 1 if term1 contained in term2 (term2 -> term1).
+    returns -1 if term2 contained in term1 (term1 -> term2).
+    returns 0 if equal.
+    else None.
+    '''
+    res = 0
+    for i in range(len(term1)):
+        comp = compare_bits(term1[i], term2[i])
+        if comp != 0:
+            if comp != res:
+                if res != 0:
+                    return None
+                else:
+                    res = comp
+    return res
+
+def is_speciel_complement(term1, term2):
+    '''
+    returns the different stable bit if term1 is contained in term2 at all bits except one.
+    otherwise returns -1.
+    '''
+    diff = diff_digits(term1, term2)
+    if len(diff) == 1:
+        for i in range(len(term1)):
+            if compare_bits(term1[i], term2[i]) == -1:
+                return -1
+        return diff.get_digit()
+    return -1
+
+def remove_literal(term, digit):
+    return term[:digit] + 'u' + term[digit+1:]
+
 class DiffDigits(object):
     def __init__(self, digits):
         self.digits = set(digits)
@@ -33,18 +65,6 @@ class DiffDigits(object):
 
     def get_digit(self):
         return next(iter(self.digits))
-
-    def __add__(self, other):
-        sum_diff = DiffDigits([])
-        digits = self.digits | other.digits
-        removed_digits = self.removed_digits | other.removed_digits
-        sum_diff.removed_digits = removed_digits
-
-        for d in digits:
-            if d not in removed_digits:
-                sum_diff.digits.add(d)
-
-        return sum_diff
 
     def __len__(self):
         return len(self.digits)
@@ -60,9 +80,10 @@ class BooleanFunction(object):
         """
         self.n = n
         self.formatting = "{" + "0:0{}b".format(n) + "}"
-        self.one_inputs = one_inputs
+        self.one_inputs = set(one_inputs)
         self.zero_inputs = [self.formatting.format(i) for i in range(2**n) if self.formatting.format(i) not in set(one_inputs)]
         self.full_matrix = self.__create_full_matrix()
+        self.partial_matrix = self.__create_partial_matrix()
 
     def calc(self, input):
         return 1 if input in self.one_inputs else 0
@@ -72,96 +93,64 @@ class BooleanFunction(object):
         for one_input in self.one_inputs:
             row = []
             for zero_input in self.zero_inputs:
-                row.append(self.__find_diff_digits(one_input, zero_input))
+                row.append(diff_digits(one_input, zero_input))
             table.append(row)
 
         x = np.array(table)
         return pd.DataFrame(x, index=self.one_inputs, columns=self.zero_inputs)
 
-    def __find_diff_digits(self, input, other_input):
-        return DiffDigits([i+1 for i in range(self.n) if input[i] != other_input[i] and input[i] != 'u' and other_input[i] != 'u'])
-
     def find_merge(self, df, axis=1):
-        inputs = df.index if axis == 1 else df.columns
-        for input1 in inputs:
-            for input2 in inputs:
-                res = self.merge(input1, input2)
+        terms = df.index if axis == 1 else df.columns
+        for term1 in terms:
+            for term2 in terms:
+                res = self.merge(term1, term2)
                 if res:
-                    new_input, drop_input, diff_digit = res
-                    series = df.loc[drop_input] if axis == 1 else df[drop_input]
+                    new_term, drop_term, diff_digit = res
+                    series = df.loc[drop_term] if axis == 1 else df[drop_term]
                     new_series = series.apply(lambda x: x.remove(diff_digit))
-                    return input1, input2, new_input, new_series, drop_input
+                    return term1, term2, new_term, new_series, drop_term
         return None
 
-    def compare_us(self, input1, input2):
+    def merge(self, term1, term2):
         '''
-        0 - equal
-        1 - input1 is bigger
-        -1 - input2 is bigger
-        None - incompatible
+        new_term, drop, diff
         '''
-        res = 0
-        for i in range(self.n):
-            if input1[i] == 'u' and input2[i] != 'u':
-                if res == 0:
-                    res = -1
-                if res == 1:
-                    return None
-            if input1[i] != 'u' and input2[i] == 'u':
-                if res == 0:
-                    res = 1
-                if res == -1:
-                    return None
-        return res
+        digit = is_speciel_complement(term1, term2)
+        if digit != -1:  # spciel complement, we can drop term1 and add u
+            return remove_literal(term1, digit), term1, digit
+        digit = is_speciel_complement(term2, term1)
+        if digit != -1:  # spciel complement, we can drop term2 and add u
+            return remove_literal(term2, digit), term2, digit
+        res = is_contained(term1, term2)
+        if res == -1:  # we can drop term2
+            return term1, term2, None
+        if res == 1:  # we can drop term1
+            return term2, term1, None
+        return None # cannot merge
 
-    def merge(self, input1, input2):
-        '''
-        new_input, drop, diff
-        '''
-        diff = self.__find_diff_digits(input1, input2)
-        if len(diff) > 1:
-            return None
-        comp_u = self.compare_us(input1, input2)
-        if comp_u == None:
-            return None
-        '''
-        inputs can be complement/included
-        '''
-        if len(diff) == 0: # included
-            if comp_u == 1: # input2 has more u's
-                return input2, input1, None
-            if comp_u == -1: # input1 has more u's
-                return input1, input2, None
-        else: # complement
-            if comp_u == 1 or comp_u == 0:  # input2 has more u's
-                return input1[:diff.get_digit()-1] + 'u' + input1[diff.get_digit():], input1, diff.get_digit()
-            if comp_u == -1:  # input1 has more u's
-                return input2[:diff.get_digit()-1] + 'u' + input2[diff.get_digit():], input2, diff.get_digit()
-
-        return None
-
-    def get_merged_matrix(self):
+    def __create_partial_matrix(self):
         df = self.full_matrix
         # merge rows
         res = self.find_merge(df, axis=1)
         while res:
-            input1, input2, new_input, new_series, drop_input = res
-            df = df.drop(index=list([drop_input]))
-            df.loc[new_input] = new_series
+            term1, term2, new_term, new_series, drop_term = res
+            df = df.drop(index=list([drop_term]))
+            df.loc[new_term] = new_series
             res = self.find_merge(df, axis=1)
-        # transpose?!?
         # merge cols
         res = self.find_merge(df, axis=0)
         while res:
-            input1, input2, new_input, new_series, drop_input = res
-            df = df.drop(columns=list([drop_input]))
-            df[new_input] = new_series
+            term1, term2, new_term, new_series, drop_term = res
+            df = df.drop(columns=list([drop_term]))
+            df[new_term] = new_series
             res = self.find_merge(df, axis=0)
         return df
 
 def main():
-    f = BooleanFunction(3, ['001', '101', '110'])
-    f.get_merged_matrix()
+    f = BooleanFunction(3, ['111', '101', '010', '011']) # mux
+    print(f.full_matrix)
+    print(f.partial_matrix)
+    # not working on mux!
 
 if __name__ == '__main__':
     main()
