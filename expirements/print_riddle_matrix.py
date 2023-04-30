f8b66dd21807f19aa2e3eca2b7a7ca542e617550
Leah-London-Arazi <leahl@mail.tau.ac.il>
2023-04-30 10:32:53 +0300

    added hazard derivative

--- expirements/print_riddle_matrix.py
+++ expirements/print_riddle_matrix.py
@@ -60,6 +60,21 @@ def is_complement(term1, term2):
 def remove_literal(term, digit):
     return term[:digit] + 'u' + term[digit+1:]
 
+def substitute(term, stable_bits):
+    bits = list(term)
+    j = 0
+    for i, bit in enumerate(term):
+        if bit == 'u':
+            bits[i] = stable_bits[j]
+            j += 1
+    return "".join(bits)
+
+def all_resolutions(term):
+    bits = list(term)
+    u_count = bits.count('u')
+    stable_format = "{" + "0:0{}b".format(u_count) + "}"
+    return [substitute(term, stable_format.format(i)) for i in range(2 ** u_count)]
+
 class BooleanFunction(object):
     def __init__(self, n, one_inputs):
         """
@@ -73,6 +88,19 @@ class BooleanFunction(object):
         self.full_matrix = self.__create_full_matrix()
         self.partial_matrix = self.__create_partial_matrix()
 
+    def is_monotone(self):
+        '''
+        for all x,y if x<=y then f(x)<=f(y).
+        or, not(exists x<=y such that f(x)>f(y)).
+        '''
+        zeros = set(self.zero_inputs)
+        for one_input in self.one_inputs:
+            for i, bit in enumerate(one_input):
+                if bit == '0':
+                    if (one_input[:i] + '1' + one_input[i+1:]) in zeros:
+                        return False
+        return True
+
     def calc(self, input):
         return 1 if input in self.one_inputs else 0
 
@@ -85,6 +113,8 @@ class BooleanFunction(object):
             table.append(row)
 
         x = np.array(table)
+        if len(rows) == 0 or len(columns) == 0:
+            return pd.DataFrame(x)
         return pd.DataFrame(x, index=rows, columns=columns)
 
     def __create_full_matrix(self):
@@ -138,22 +168,89 @@ class BooleanFunction(object):
         zero_terms = self.merge_terms(self.zero_inputs)
         return self.__create_matrix(one_terms, zero_terms)
 
-def main():
-    f = BooleanFunction(3, ['111', '101', '010', '011']) # mux
-    print(f.full_matrix)
-    print(f.partial_matrix)
-    f = BooleanFunction(3, ['001', '101', '010'])
-    print(f.full_matrix)
-    print(f.partial_matrix)
-    f = BooleanFunction(3, ['001', '011', '111']) # monotone
-    print(f.full_matrix)
-    print(f.partial_matrix)
-    f = BooleanFunction(3, ['001', '000', '111']) # there is a gap!
-    print(f.full_matrix)
-    print(f.partial_matrix)
-    f = BooleanFunction(3, ['010', '000', '111']) # there is a gap!
-    print(f.full_matrix)
-    print(f.partial_matrix)
+    def get_derivative(self, x, y):
+        term = "".join(['u' if y[i] == '1' else x[i] for i in range(len(x))])
+        resolutions = all_resolutions(term)
+        f_on_x = self.calc(x)
+        for res in resolutions:
+            if self.calc(res) != f_on_x:
+                return 1
+        return 0
 
+def str_communication_matrices(f, header, as_html=True):
+    string = "################ {} ################".format(header) + "\n" + \
+             (f.full_matrix.to_html() if as_html else f.full_matrix) + "\n\n" + \
+             (f.partial_matrix.to_html() if as_html else f.partial_matrix) + "\n"
+    return string
+
+def all_possible_matrices(n):
+    '''
+    no complement functions! ('0...0' is fixed to 0)
+    '''
+    functions = []
+    formatting = "{" + "0:0{}b".format(n) + "}"
+    func_formatting = "{" + "0:0{}b".format(2 ** n) + "}"
+    inputs = [formatting.format(i) for i in range(2 ** n)]
+    for i in range(2 ** (2 ** n - 1)):
+        func = func_formatting.format(i)
+        f = BooleanFunction(n, [inputs[k] for k in range(2 ** n) if func[k] == '1'])
+        functions.append(f)
+    return functions
+
+def print_all_possible_matrices(n, monotone=False, to_file=False, file_path=""):
+    func_formatting = "{" + "0:0{}b".format(2 ** n) + "}"
+    functions = all_possible_matrices(n)
+    string = "\n".join([str_communication_matrices(f, str(i) + " | " + func_formatting.format(i), as_html=to_file) for i,f in enumerate(functions) if f.is_monotone() or monotone])
+    if to_file:
+        with open(file_path, "w") as file:
+            file.write(string)
+    else:
+        print(string)
+
+def main():
+    # output_files_dir = r'C:\Users\Leah London Arazi\Dropbox\My PC (Leah-X1-Carbon)\Documents\Leah X1 Carbon\university\2023b\first steps in research\hazard free circuits\first_steps_in_reasearch\expirements'
+    # print_all_possible_matrices(3, monotone=True, to_file=True, file_path=output_files_dir + "\\all_functions_3_inputs_with monotone.html")
+    # functions = all_possible_matrices(3)
+    # for i,f in enumerate(functions):
+    #     if not f.is_monotone():
+    #         print_communication_matrices(f, str(i), to_file=True, file_path=output_file_path)
+    # output_file_path = r'C:\Users\Leah London Arazi\Dropbox\My PC (Leah-X1-Carbon)\Documents\Leah X1 Carbon\university\2023b\first steps in research\hazard free circuits\first_steps_in_reasearch\expirements\all_functions_4_inputs.html'
+    # functions = all_possible_matrices(4)
+    # for i,f in enumerate(functions):
+    #     if not f.is_monotone():
+    #         print_communication_matrices(f, str(i), to_file=True, file_path=output_file_path)
+    '''
+    trying to find a connection to the hazard derivative
+    '''
+    # 10
+    f = BooleanFunction(3, ['000', '001', '010', '011', '101', '111'])
+    der = []
+    for i in range(2 ** 3):
+        y = f.formatting.format(i)
+        if f.get_derivative('000', y) == 1:
+            der.append(y)
+    derivative = BooleanFunction(3, der)
+    print(derivative.partial_matrix)
+
+    # 12 (switching third and second digit from 10)
+    f = BooleanFunction(3, ['000', '001', '010', '011', '110', '111'])
+    der = []
+    for i in range(2 ** 3):
+        y = f.formatting.format(i)
+        if f.get_derivative('000', y) == 1:
+            der.append(y)
+    derivative = BooleanFunction(3, der)
+    print(derivative.partial_matrix)
+
+    # 3 (monotone, we can get it from 10 by switching the second and the third digit, and then flipping the second digit)
+    f = BooleanFunction(3, ['000', '001', '010', '011', '100', '101'])
+    der = []
+    for i in range(2 ** 3):
+        y = f.formatting.format(i)
+        if f.get_derivative('010', y) == 1:
+            der.append(y)
+    derivative = BooleanFunction(3, der)
+    print(derivative.partial_matrix)
+    
 if __name__ == '__main__':
     main()
